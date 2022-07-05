5631d8d7d448f0e8dd82164c90f54b1828a12408
Ninja77030 <n77030@outlook.com>
2022-07-05 18:50:56 +0530

    Enabled JavaScript, which I forgot to enable

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+            javascript: true
+        }
+    });
+    //win.loadURL("http://localhost:8080");
+    win.loadFile(`${__dirname}\\bin\\index.html`);
+    win.removeMenu();
+}
+
+app.on('ready', createWindow);
+
+app.on('quit', () => {
+    app.quit();
+});
